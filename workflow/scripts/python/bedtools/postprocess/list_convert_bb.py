34066ca5fe710bfbed79c52397d8eba99f6ce21c
ndwarshuis <ndwar@yavin4.ch>
2024-06-10 21:26:26 -0400

    FIX bb error

--- workflow/scripts/python/bedtools/postprocess/list_convert_bb.py
+++ workflow/scripts/python/bedtools/postprocess/list_convert_bb.py
@@ -6,10 +6,10 @@ import subprocess as sp
 def main(smk: Any) -> None:
     bed2bb = cfg.smk_to_input_name(smk, "bed2bb")
     genome = cfg.smk_to_input_name(smk, "genome")
-    targets = cfg.smk_to_input_name(smk, "targets")
-    with open(targets, "r") as i, open(cfg.smk_to_output(smk), "w") as o:
-        for t in i:
-            outfile = t.replace(".bed", ".bb")
+    targets = cfg.smk_to_inputs_name(smk, "targets")
+    with open(cfg.smk_to_output(smk), "w") as o:
+        for t in targets:
+            outfile = str(t).replace(".bed", ".bb")
             sp.run([bed2bb, t, genome, outfile], text=True)
             o.write(outfile + "\n")
 
